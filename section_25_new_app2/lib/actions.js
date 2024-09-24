070cbe2f01a3fc860c6b26b81c68d3dea3e79afa
burak.dunal <burak.dunal@gmail.com>
2024-09-24 14:46:02 +0300

    meal demo app form submit handle and revalidateCache actions

--- section_25_new_app2/lib/actions.js
+++ section_25_new_app2/lib/actions.js
@@ -1,19 +1,36 @@
-'use server';
+"use server";
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+
+export async function shareMeal(prevState, formData) {
   const meal = {
-    title: formData.get('title'),
-    summary: formData.get('summary'),
-    instructions: formData.get('instructions'),
-    image: formData.get('image'),
-    creator: formData.get('name'),
-    creator_email: formData.get('email'),
-    slug:''
+    title: formData.get("title"),
+    summary: formData.get("summary"),
+    instructions: formData.get("instructions"),
+    image: formData.get("image"),
+    creator: formData.get("name"),
+    creator_email: formData.get("email"),
   };
 
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    !meal.image.size === 0
+  ) {
+    return { message: "Invalid input." };
+  }
   await saveMeal(meal);
-  redirect('/meals');
+  revalidatePath("/meals"); //revalidate içerisine verilen pathi cache'de yeniler. eğer ('/', 'layout') şeklinde verilirse tüm path'leri layouta sarılı tüm sayfaların yeni hallerini yükler
+  redirect("/meals");
 }
